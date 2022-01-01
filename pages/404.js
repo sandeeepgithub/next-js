620f5aa763f414a7bae6774016152c573998fffd
Sandeep <sandeeep.modi@github.com>
2022-01-01 19:47:14 +0530

    6-Custom 404 error

---
+++ pages/404.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { FaExclamation } from "react-icons/fa";
+
+import Layout from "../components/Layout";
+import styles from "../styles/404.module.css";
+
+function NotFoundPage() {
+  return (
+    <Layout title="Page not found">
+      <div className={styles.error}>
+        <h1>
+          {" "}
+          <FaExclamation /> 404{" "}
+        </h1>
+        <h4> Sorry there is nothing here </h4>
+        <Link href="/"> Go back home</Link>
+      </div>
+    </Layout>
+  );
+}
+
+export default NotFoundPage;
