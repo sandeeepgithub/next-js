215e8b5e3d7a3ec2969795ad9bcac1c746cce7da
Sandeep <sandeeep.modi@github.com>
2022-01-01 17:48:47 +0530

    3-Next Link and router

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 function AboutPage() {
   return (
     <div>
       <h1> This is about page </h1>
+      <Link href="/">Home</Link>
     </div>
   );
 }
--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,14 @@
+import { useRouter } from "next/router";
+
 function EventPage() {
+  const router = useRouter();
+  console.log(router);
+
   return (
     <div>
       <h1> Event page </h1>
+      <h3> {router.query.slug} </h3>
+      <button onClick={() => router.push("/")}> Click</button>
     </div>
   );
 }
