b7561b23c5f5068279fb353d5c6f9f4900f1c467
Sandeep <sandeeep.modi@github.com>
2022-01-04 15:50:00 +0530

    4 - getStaticPaths

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,11 +1,57 @@
+import axios from "axios";
+import { API_URL } from "@/config/index";
 import Layout from "../../components/Layout";
 
-function EventPage() {
+function EventPage({ data }) {
+  console.log(data);
   return (
     <Layout>
       <h1> Event page </h1>
+      <h3> {data.name} </h3>
     </Layout>
   );
 }
 
 export default EventPage;
+
+export const getStaticPaths = async () => {
+  let data;
+
+  await axios.get(`${API_URL}/api/events`).then((res) => (data = res.data));
+
+  let paths = data.map((evt) => ({
+    params: { slug: evt.slug },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+export const getStaticProps = async ({ params: { slug } }) => {
+  let data;
+  await axios
+    .get(`${API_URL}/api/events/${slug}`)
+    .then((res) => (data = res.data));
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+// export const getServerSideProps = async ({ query: { slug } }) => {
+//   let data;
+
+//   await axios.get(`${API_URL}/api/events`).then((res) => (data = res.data));
+
+//   data = data.filter((evt) => evt.slug === slug)[0];
+
+//   return {
+//     props: {
+//       data,
+//     },
+//   };
+// };
