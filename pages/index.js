4fef4007a1c053d5b85eb8346469a2fbe87b861d
Sandeep <sandeeep.modi@github.com>
2022-01-04 14:28:04 +0530

    2 - getServerSide & getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,32 @@
 import Layout from "@/components/Layout";
 import styles from "@/styles/Layout.module.css";
+import { API_URL } from "@/config/index";
 
-function Home() {
+import axios from "axios";
+import EventItem from "@/components/EventItem";
+
+function Home({ data }) {
+  console.log(data);
   return (
     <Layout>
       <div className={styles.container}>
-        <h1> Home </h1>
+        {data.map((evt) => (
+          <EventItem key={evt.id} evt={evt} />
+        ))}
       </div>
     </Layout>
   );
 }
 
 export default Home;
+
+export const getServerSideProps = async () => {
+  let data;
+  await axios.get(`${API_URL}/api/events`).then((res) => {
+    data = res;
+  });
+
+  return {
+    props: data,
+  };
+};
